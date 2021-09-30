528d27a191dde639dfb13c3d441336b4bba558e6
Andrey Nikitin <tamga05@gmail.com>
2021-09-30 21:54:42 +0300

    addition

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,3 +1,12 @@
 
-
+const swiper = new Swiper('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    });
 //# sourceMappingURL=script.js.map
