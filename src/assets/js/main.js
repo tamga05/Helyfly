448c86494b0c6361fe519554739b3f3811832830
Andrey Nikitin <tamga05@gmail.com>
2021-10-01 08:30:53 +0300

    addition

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,14 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.mySwiper', {
+
+    // Pagination circles
     pagination: {
         el: '.swiper-pagination',
-        },
+    },
+
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+
     });
